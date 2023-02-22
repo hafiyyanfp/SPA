be5c1b12e7b75920f23a611ce3c05080dd6c49b1
akuian <hfazapratamahf@gmail.com>
2023-02-22 10:53:28 +0700

    state

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function Link(props) {
+let state = {
+    hash: window.location.hash,
+    inputValue: "",
+  };
+  
+  function setState(newState) {
+    state = { ...state, ...newState };
+    render();
+  }
+  
+  function Link(props) {
     const a = document.createElement("a");
     a.href = props.href;
     a.textContent = props.label;
     a.onclick = function (event) {
       event.preventDefault();
-      history.pushState(null, "", event.target.href);
-      render();
+      const url = new URL(event.target.href);
+      setState({ hash: url.hash });
+      history.pushState(null, "", url.hash);
     };
     return a;
   }
@@ -21,34 +32,43 @@ function Link(props) {
     return div;
   }
   
-  function HomePage() {
+  function HomeScreen() {
     const navbar = Navbar();
   
     const p = document.createElement("p");
-    p.textContent = "Welcome to Home Page";
   
     const textPreview = document.createElement("p");
+    textPreview.textContent = state.inputValue;
   
     const input = document.createElement("input");
-    input.placeholder = "enter your name";
+    input.id = "input";
+    input.value = state.inputValue;
+    input.placeholder = "Enter Your Name";
     input.oninput = function (event) {
-      textPreview.textContent = event.target.value;
+      setState({ inputValue: event.target.value });
+    };
+  
+    const buttonClear = document.createElement("button");
+    buttonClear.textContent = "Clear";
+    buttonClear.onclick = function () {
+      setState({ inputValue: "" });
     };
   
     const div = document.createElement("div");
     div.append(navbar);
     div.append(p);
     div.append(input);
+    div.append(buttonClear);
     div.append(textPreview);
   
     return div;
   }
   
-  function AboutPage() {
+  function AboutScreen() {
     const linkHome = Link({ href: "#home", label: "Kembali Ke Home" });
   
     const p = document.createElement("p");
-    p.textContent = "Welcome to About Page";
+    p.textContent = "Welcome to About";
   
     const div = document.createElement("div");
     div.appendChild(linkHome);
@@ -57,23 +77,35 @@ function Link(props) {
   }
   
   function App() {
-    const homePage = HomePage();
-    const aboutPage = AboutPage();
+    const homeScreen = HomeScreen();
+    const aboutScreen = AboutScreen();
   
-    if (window.location.hash == "#home") {
-      return homePage;
-    } else if (window.location.hash == "#about") {
-      return aboutPage;
+    if (state.hash == "#home") {
+      return homeScreen;
+    } else if (state.hash == "#about") {
+      return aboutScreen;
     } else {
-      return homePage;
+      return homeScreen;
     }
   }
   
   function render() {
     const root = document.getElementById("root");
     const app = App();
+
+    const focusedElementId = document.activeElement.id;
+    const focusedElementSelectionStart = document.activeElement.selectionStart;
+    const focusedElementSelectionEnd = document.activeElement.selectionEnd;
+  
     root.innerHTML = "";
     root.appendChild(app);
+  
+    if (focusedElementId) {
+      const focusedElement = document.getElementById(focusedElementId);
+      focusedElement.focus();
+      focusedElement.selectionStart = focusedElementSelectionStart;
+      focusedElement.selectionEnd = focusedElementSelectionEnd;
+    }
   }
   
   render();
